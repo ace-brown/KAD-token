e3533dd07a4dffe68438c971ff687e20f8c9c774
ace-brown <kazem_abdi@yahoo.com>
2024-09-23 08:19:22 +0200

    Deploy script added

--- scripts/1_deploy.ts
+++ scripts/1_deploy.ts
@@ -1,6 +1,39 @@
-import { clusterApiUrl, Connection } from '@solana/web3.js'
-import { checkOwner } from './reuse'
+import fs from 'fs'
+import { clusterApiUrl, Connection, Keypair } from '@solana/web3.js'
+import { createMint } from '@solana/spl-token'
+
+import { checkOwner, getTokenAddress, OWNER } from './reuse'
 
 checkOwner()
 
 const connection = new Connection(clusterApiUrl('devnet'))
+
+async function createToken(OWNER: Keypair): Promise<void> {
+  // createMint creates and initializes a new mint
+  const tokenMint = await createMint(connection, OWNER, OWNER.publicKey, null, 2)
+  console.log(`✅ Token mint created: ${tokenMint.toString()}`)
+
+  // Write token mint to a file
+  const address = JSON.stringify({ address: tokenMint.toString() }, null, 4)
+  fs.writeFile('./services/tokenMint.json', address, 'utf8', (error) => {
+    if (error) {
+      console.error('Error saving tokenMint address: ', error)
+    } else {
+      console.log('✅ Deployed tokenMint address: ', address)
+    }
+  })
+}
+
+async function main() {
+  await createToken(OWNER)
+  setTimeout(() => {
+    try {
+      const tokenMint = getTokenAddress()
+      console.log('✅ Finished! Deployment complete!', tokenMint)
+    } catch (error) {
+      console.error('Error reading tokenMint file: ', error)
+    }
+  }, 2000)
+}
+
+main()
--- scripts/reuse.ts
+++ scripts/reuse.ts
@@ -1,10 +1,10 @@
+import fs from 'fs'
 import 'dotenv/config'
 import path from 'path'
-import fs from 'fs'
 import { Keypair, PublicKey } from '@solana/web3.js'
 
 export const TOKEN_OWNER = process.env.INJECTED_DEV_TOKEN_OWNER_KEYPAIR || ''
-export const checkOwner = () => {
+export function checkOwner() {
   if (!TOKEN_OWNER) {
     console.log('Please provide a TOKEN OWNER KEYPAIR')
     process.exit(1)
@@ -12,15 +12,21 @@ export const checkOwner = () => {
 }
 
 // Converting owenr array (comming from dotenv) into a keyPair
-export const ownerArray = Uint8Array.from(TOKEN_OWNER.split(',').map(Number))
+const ownerArray = Uint8Array.from(TOKEN_OWNER.split(',').map(Number))
 export const OWNER: Keypair = Keypair.fromSecretKey(ownerArray)
 
-export const getTokenAddress = (): PublicKey => {
-  const filePath = path.join(__dirname, '../services/minToken.json')
+export function getTokenAddress(): PublicKey {
+  const filePath = path.join(__dirname, '../services/tokenMint.json')
   if (!fs.existsSync(filePath)) {
     throw new Error('No Token mint file found')
   }
-
+  /**
+   * In Webster, a mint means a place where money is coined, In blockchain ecosystems,
+   * a "mint" typically refers to a special account or object that governs a token
+   * type. This mint account holds the authority to mint (create) new tokens.
+   * The term "mint" is not describing an action like "minting tokens," but rather
+   * it refers to the source or origin of the tokens.
+   */
   const tokenMintString = fs.readFileSync(filePath, 'utf-8')
   const tokenMint = JSON.parse(tokenMintString)
   return new PublicKey(tokenMint.address)
